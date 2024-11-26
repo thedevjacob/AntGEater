bc4a96af01e17bea958044c2db59841815d00ce0
Jacob <jsoryal@uci.edu>
2024-11-25 16:45:30 -0800

    gets all courses in a certain term and under a certain GE

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import requests, json
 import user_info
 
-year, quarter, department, course_number = user_info.get_all_course_info()
-print(f"https://api.peterportal.org/rest/v0/schedule/soc?term={year}%20{quarter}&department={department}&courseNumber={course_number}")
-all_courses = requests.get(f"https://api.peterportal.org/rest/v0/schedule/soc?term={year}%20{quarter}&department={department}&courseNumber={course_number}").json()
+# all_courses = requests.get(f"https://api.peterportal.org/rest/v0/schedule/soc?term={year}%20{quarter}&department={department}&courseNumber={course_number}").json()
 
-# # course_code = int(input("Course code <<< "))
-# course_code = all_courses
+year, quarter, GE_category = user_info.get_all_GE_info()
+all_courses = requests.get(f"https://api.peterportal.org/rest/v0/schedule/soc?term={year}%20{quarter}&ge={GE_category}").json()
+
 # all_courses = requests.get(f"https://api.peterportal.org/rest/v0/grades/calculated?code={course_code}").json()
 # average_gpa = all_courses['gradeDistribution']['average_gpa']
 
-file = open("stuff.json", 'w')
-json.dump(all_courses, file, indent = 4) # type: ignore
+# file = open("courses.json", 'w')
+# json.dump(all_courses, file, indent = 4) # type: ignore
 
-relevant_coursed = []
-for course in all_courses["courseList"]:
-    print(course["year"])
-
+# relevant_coursed = []
+# for course in all_courses["courseList"]:
+#     print(course["year"])
 
 # print(round(average_gpa, 2))
--- user_info.py
+++ user_info.py
@@ -1,3 +1,5 @@
+# all input functions
+
 def get_all_course_info() -> tuple[int, str, str, str]:
     # get all info
     year = get_year()
@@ -6,6 +8,14 @@ def get_all_course_info() -> tuple[int, str, str, str]:
     course_number = get_course_number()
 
     return year, quarter, department, course_number
+
+def get_all_GE_info() -> tuple[int, str, str]:
+    # get all info
+    year = get_year()
+    quarter = get_quarter()
+    GE_category = get_course_GE()
+
+    return year, quarter, GE_category
 
 def get_year() -> int:
     return int(input("Year <<< "))
@@ -26,3 +36,19 @@ def get_department() -> str:
 
 def get_course_number() -> str:
     return input("Course Number <<< ")
+
+def get_course_GE() -> str:
+    GE_categories = ['ANY', 'GE-1A', 'GE-1B', 'GE-2', 'GE-3', 'GE-4',
+                     'GE-5A', 'GE-5B', 'GE-6', 'GE-7', 'GE-8']
+
+    print("Which GE category would you like to select?")
+    for category in GE_categories:
+        print(f" • {category}")
+    category_picked = input(" GE <<< ").upper().strip()
+
+    if category_picked in GE_categories: return category_picked
+    if ("GE-" + category_picked) in GE_categories: return "GE-" + category_picked
+
+    print(f"Please correct only a listed category. You picked: '{category_picked}'")
+    return get_course_GE()
+
